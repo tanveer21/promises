a39281bfb72ac67c68601053de9e575136e27dbd
prikshitmaurya <pm.sebiz@gmail.com>
2019-07-25 18:28:41 +0530

    custom treasure promise implemented

--- promisecustom.test.js
+++ promisecustom.test.js
@@ -2,16 +2,18 @@ const Treasure = require('./promisecustom.js');
 
 
 describe('Treasure' , function(){
+    
 
     it('should exist' , function(){
         expect(Treasure).toBeDefined();
     });
 
+
     it('should be a function' , function(){
         expect(typeof Treasure ).toBe("function");
     });
 
-    
+
 
 
 })
